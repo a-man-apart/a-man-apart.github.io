27db42b284d2de8a5a1c9a8da5d9dd4ef150a87f
a-man-apart <114430140+a-man-apart@users.noreply.github.com>
2022-10-28 11:06:23 +0300

    Update scriptweather.js

--- scriptweather.js
+++ scriptweather.js
@@ -1,8 +1,24 @@
 //const key = "50e72e52ec3c907f32fdf091ccb0ed10";
 //const key = "7d0265ac9bfc1740a1879239981cec78";
 const key = "b40375fbb2f5e343f56a0e8e313ad655";
-const cnt = 9;
+let cnt;
+let period = document.querySelector(".selected-period");
+console.log("Perioada: ", period.value);
 
+ if (period.value == "12h") {
+    cnt = 4;
+ } else if (period.value == "24h") {
+    cnt = 8;
+    document.getElementById("container-1").style.display = "flex";
+    document.getElementById("container-2").style.display = "flex";
+    document.getElementById("container-3").style.display = "none";
+    document.getElementById("container-4").style.display = "none";
+ } else if (period.value == "36h") {
+    cnt = 12;
+ } else {
+    cnt = 16;
+ };
+ console.log("CNT: ", cnt);
 
 const select = document.querySelector(".form-select");
 
@@ -100,49 +116,49 @@ const getCoordinates = (cityName = "Brasov") => {
                     console.log("Hour: ", hour);
                     time.innerText = hour;
                        
-                    let array = ["00:00:00", "03:00:00", "06:00:00", "09:00:00", "12:00:00", "15:00:00", "18:00:00", "21:00:00", "00:00:00", "03:00:00", "06:00:00", "09:00:00", "12:00:00", "15:00:00", "18:00:00", "21:00:00"];
+                    let array = ["00:00:00", "03:00:00", "06:00:00", "09:00:00", "12:00:00", "15:00:00", "18:00:00", "21:00:00", "00:00:00", "03:00:00", "06:00:00", "09:00:00", "12:00:00", "15:00:00", "18:00:00", "21:00:00", "00:00:00", "03:00:00", "06:00:00", "09:00:00", "12:00:00", "15:00:00", "18:00:00", "21:00:00"];
                         
                     let ora = hour.slice(0, 2);
                     console.log("Ora: ", ora);
 
                         if (ora >= 00 && ora <= 02) {
-                            for (let i = 0; i < interval.length; i++) {
+                            for (let i = 0; i < cnt; i++) {
                                 interval[i].innerText = `${array[i]} - ${array[i+1]}`;
                             };
                         } else if (ora >= 03 && ora <= 05) {
-                            for (let i = 1; i < (interval.length+1); i++) {
+                            for (let i = 1; i < (cnt+1); i++) {
                                 interval[i-1].innerText = `${array[i]} - ${array[i+1]}`;
                             };
                         } else if (ora <= 06 && ora <= 08) {
-                            for (let i = 2; i < (interval.length+2); i++) {
+                            for (let i = 2; i < (cnt+2); i++) {
                                 interval[i-2].innerText = `${array[i]} - ${array[i+1]}`;
                             }; 
                         } else if (ora >= 09 && ora <= 11) {
-                            for (let i = 3; i < (interval.length+3); i++) {
+                            for (let i = 3; i < (cnt+3); i++) {
                                 interval[i-3].innerText = `${array[i]} - ${array[i+1]}`;
                             }; 
                         } else if (ora >= 12 && ora <= 14) {
-                            for (let i = 4; i < (interval.length+4); i++) {
+                            for (let i = 4; i < (cnt+4); i++) {
                                 interval[i-4].innerText = `${array[i]} - ${array[i+1]}`;
                             };
                         } else if (ora >= 15 && ora <= 17) {
-                            for (let i = 5; i < (interval.length+5); i++) {
+                            for (let i = 5; i < (cnt+5); i++) {
                                 interval[i-5].innerText = `${array[i]} - ${array[i+1]}`;
                             }; 
                         } else if (ora >= 18 && ora <= 20) {
-                            for (let i = 6; i < (interval.length+6); i++) {
+                            for (let i = 6; i < (cnt+6); i++) {
                                 interval[i-6].innerText = `${array[i]} - ${array[i+1]}`;
                             };
                         } else if (ora >= 21 && ora <= 23) {
-                            for (let i = 7; i < (interval.length+7); i++) {
+                            for (let i = 7; i < (cnt+7); i++) {
                                 interval[i-7].innerText = `${array[i]} - ${array[i+1]}`;
                             };
                         };
                     //interval[i].innerText = `${data.list[i].dt_txt.slice(11)} - ${data.list[i+1].dt_txt.slice(11)}`;
                     //time.innerText = `${date.toLocaleDateString()} - ${date.toLocaleTimeString()}`;
-                    for (let i = 0; i < interval.length; i++) {
-                        // interval[i].innerText = "12:00:00 - 15:00:00";
-                    };
+                    // for (let i = 0; i < interval.length; i++) {
+                    //     // interval[i].innerText = "12:00:00 - 15:00:00";
+                    // };
                       
                         console.log('Success:', data);
                     })
@@ -185,15 +201,15 @@ const getWeather = (cityId = 683844) => {
             city[i].innerText = data.city.name;
         }
         //temp.innerText = kelvinToCelsius(data.list[0].main.temp) + `\xB0C`;
-        for (let i = 0; i < temp.length; i++) {
+        for (let i = 0; i < cnt; i++) {
             temp[i].innerText = kelvinToCelsius(data.list[i].main.temp) + `\xB0C`;
         }
         //icon.src = `http://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png`;
-        for (let i = 0; i < icon.length; i++) {
+        for (let i = 0; i < cnt; i++) {
             icon[i].src = `http://openweathermap.org/img/wn/${data.list[i].weather[0].icon}@2x.png`;
         }
         weatherType.innerText = firstToUppercase1 (data.list[0].weather[0].description);
-        for (let i = 0; i < weatherType.length; i++) {
+        for (let i = 0; i < cnt; i++) {
             weatherType[i].innerText = firstToUppercase1 (data.list[i].weather[0].description);
         }
 
@@ -215,15 +231,15 @@ const getWeather = (cityId = 683844) => {
 
 
         //wind.innerText = `Wind: ${data[0].wind.speed} m/s`;
-        for (let i = 0; i < wind.length; i++) {
+        for (let i = 0; i < cnt; i++) {
             wind[i].innerText = `Wind: ${data.list[i].wind.speed} m/s`;
         }
         //humidity.innerText = `Humidity: ${data[0].main.humidity}%`;
-        for (let i = 0; i < humidity.length; i++) {
+        for (let i = 0; i < cnt; i++) {
             humidity[i].innerText = `Humidity: ${data.list[i].main.humidity}%`;
         }
         //atmp.innerText = `Pressure: ${data[0].main.pressure} hPa`;
-        for (let i = 0; i < atmp.length; i++) {
+        for (let i = 0; i < cnt; i++) {
             atmp[i].innerText = `Pressure: ${data.list[i].main.pressure} hPa`;
         }
 
@@ -239,12 +255,47 @@ getWeather();
 getCoordinates();
 
 
+let cityId;
+let cityName;
 
 select.addEventListener("change", function(e) {
-    const cityId = e.target.value;
-    const cityName = e.target[e.target.selectedIndex].text;
+    cityId = e.target.value;
+    cityName = e.target[e.target.selectedIndex].text;
     console.log("cityId: ", cityId);
     console.log("cityName: ", cityName);
+    getWeather(cityId);
+    getCoordinates(cityName);
+});
+
+
+
+period.addEventListener("change", function(e) {
+    if (period.value == "12h") {
+        cnt = 4;
+        document.getElementById("container-1").style.display = "flex";
+        document.getElementById("container-2").style.display = "none";
+        document.getElementById("container-3").style.display = "none";
+        document.getElementById("container-4").style.display = "none";
+     } else if (period.value == "24h") {
+        cnt = 8;
+        document.getElementById("container-1").style.display = "flex";
+        document.getElementById("container-2").style.display = "flex";
+        document.getElementById("container-3").style.display = "none";
+        document.getElementById("container-4").style.display = "none";
+     } else if (period.value == "36h") {
+        cnt = 12;
+        document.getElementById("container-1").style.display = "flex";
+        document.getElementById("container-2").style.display = "flex";
+        document.getElementById("container-3").style.display = "flex";
+        document.getElementById("container-4").style.display = "none";
+     } else if (period.value == "48h") {
+        cnt = 16;
+        document.getElementById("container-1").style.display = "flex";
+        document.getElementById("container-2").style.display = "flex";
+        document.getElementById("container-3").style.display = "flex";
+        document.getElementById("container-4").style.display = "flex";
+     };
+     console.log("CNT: ", cnt);
     getWeather(cityId);
     getCoordinates(cityName);
 });
